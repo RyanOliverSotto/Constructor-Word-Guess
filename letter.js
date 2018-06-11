438c5af5ced3a87467d7b88ca61a8011f87cdf79
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-11 16:39:03 -0500

    Adding Readme

--- letter.js
+++ letter.js
@@ -6,6 +6,7 @@ depending on whether or not the user has guessed the letter. That means the cons
   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 */
+debugger
 function Letter(letter){
     this.letter = letter;
     this.guessed = false;
